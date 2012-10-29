b55d1c7e7852120369239fa536b60176652c2c0d
Elliott Friedman <elliott.friedman@gmail.com>
2012-10-28 18:47:06 -0700

    fixing shit

--- scripts/tmux_loadavg.sh
+++ scripts/tmux_loadavg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-test "$(uname)" = "Darwin" && \
-  uptime | awk -F'load averages: ' '{print $NF}' || \
+test "${OS_TYPE}" = "Darwin" && \
+  uptime | awk -F'load average: ' '{gsub(/,/, ""); print $NF}' || \
   cut -f1-3 -d' ' /proc/loadavg
