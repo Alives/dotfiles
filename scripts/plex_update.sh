46c8c307cdac540fb8465e33b57e86aa22dfea95
Elliott Friedman <elliott.friedman@gmail.com>
2012-10-28 19:24:02 -0700

    Way better Plex update method that doesnt require Plex Media Server to be running already... (I think at least)

--- scripts/plex_update.sh
+++ scripts/plex_update.sh
@@ -1,4 +1,2 @@
-for entry in $(seq 1 10); do
-  /usr/local/homebrew/bin/wget -qO /dev/null --timeout=1 \
-    "http://127.0.0.1:32400/library/sections/${entry}/refresh" &
-done
+#!/bin/sh
+/Applications/Plex\ Media\ Server.app/Contents/MacOS/Plex\ Media\ Scanner -s
