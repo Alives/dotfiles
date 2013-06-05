d44dd7627af4848bdffa272e65bb51272365a1b5
Elliott Friedman <elliott.friedman@gmail.com>
2013-06-05 14:35:06 -0700

    cleaner tm

---
+++ scripts/check_ssh.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import socket
+from os.path import expanduser
+from sys import argv, stdout
+from time import sleep
+
+
+def ParseSSHConfig(target):
+  host = None
+  port = 22
+  hostname = target
+  home = expanduser("~")
+  conf = '%s/.ssh/config' % home
+  ssh_config = open(conf, 'r')
+  for line in ssh_config:
+    line = line.strip()
+    if not host and 'Host ' in line:
+      host = line.split('Host ')[-1]
+    elif host and 'HostName ' in line:
+      hostname = line.split('HostName ')[-1]
+    elif host and 'Port ' in line:
+      port = int(line.split('Port ')[-1])
+    elif len(line) == 0:
+      if ((host and hostname and port) and
+            (host == target)):
+        return (hostname, port)
+      host = None
+      hostname = target
+      port = 22
+  return (hostname, port)
+
+
+def TestConnectivity((hostname, port)):
+  try:
+    conn = socket.create_connection((hostname, port), 1)
+  except:
+    return False
+  response = conn.recv(1024)
+  conn.close()
+  if 'SSH' in response:
+    return True
+  return False
+
+
+def main():
+  count = 0
+  spinner = '-\\|/'
+  host = argv[1]
+  localhost = ParseSSHConfig('localhost')
+  target = ParseSSHConfig(host)
+  while True:
+    for host in [target, localhost]:
+      if TestConnectivity(host):
+        exit(0)
+    stdout.write('\r%c' % (spinner[count % 4]))
+    stdout.flush()
+    count += 1
+    sleep(0.5)
+
+
+if __name__ == '__main__':
+  main()
