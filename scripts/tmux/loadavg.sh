2fa38124c3854d1476ad00d3ea05d38202308025
Elliott Friedman <elliott.friedman@gmail.com>
2014-01-20 23:24:13 -0800

    adding network stats

--- scripts/tmux/loadavg.sh
+++ scripts/tmux/loadavg.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh -e
 test "${OS_TYPE}" = "Darwin" && \
   uptime | awk -F'load average: ' '{gsub(/,/, ""); print $NF}' || \
   cut -f1-3 -d' ' /proc/loadavg
---
+++ scripts/tmux/network.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+TMUX_SESSION=$(tmux list-session | awk -F: '/attached/ {print $1}')
+NETWORK_CACHE=/tmp/tmux_network_cache.${TMUX_SESSION}
+
+function scale {
+  rate=$1
+  if [ ${rate} -gt 1073741824 ]; then
+    units="GB/s"
+    rate=$((${rate}/1073741824))
+  elif [ ${rate} -gt 1048576 ]; then
+    units="MB/s"
+    rate=$((${rate}/1048576))
+  elif [ ${rate} -gt 1024 ]; then
+    units="KB/s"
+    rate=$((${rate}/1024))
+  else
+    # I don't care about b/s.
+    units="KB/s"
+    rate=0
+  fi
+  return
+}
+
+read prev_time prev_rbytes prev_tbytes <<< $(cat ${NETWORK_CACHE} 2>/dev/null || echo "0 0 0")
+read curr_rbytes curr_tbytes <<< $(awk '/eth0/ {print $2" "$10}' /proc/net/dev)
+curr_time=$(date +%s)
+echo -n "${curr_time} ${curr_rbytes} ${curr_tbytes}" > ${NETWORK_CACHE}
+[ ${prev_time} -eq 0 ] && exit
+d_tbytes=$((${curr_tbytes} - ${prev_tbytes}))
+d_rbytes=$((${curr_rbytes} - ${prev_rbytes}))
+d_time=$((${curr_time} - ${prev_time}))
+[ ${d_time} -eq 0 ] && exit
+rbytes=$(($d_rbytes / $d_time))
+tbytes=$(($d_tbytes / $d_time))
+
+scale ${rbytes}
+printf "↓%0.0f%s " ${rate} ${units}
+scale ${tbytes}
+printf "↑%0.0f%s\n" ${rate} ${units}
