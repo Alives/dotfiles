4948ca33c57636779c7973a47193ac5d2d386767
Elliott Friedman <elliott.friedman@gmail.com>
2018-05-14 09:41:46 -0700

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,9 @@
-#!/bin/bash -e
-REPO=$(git rev-parse --show-toplevel)
-path=$(basename ${REPO})
+#!/bin/bash -ex
+for name in bashrc tmux.conf vim vimrc zshrc; do
+  ln -svfT ${HOME}/.dotfiles/$name ${HOME}/.$name
+done
 
-
-function Deploy {
-  entry=$(basename $1)
-  if [ -e ${HOME}/.${entry} ]; then
-    mkdir ${HOME}/backups 2>/dev/null
-    mv -v ${HOME}/.${entry} ${HOME}/backups/${entry}
-  fi
-  ln -sv ${path}/${entry} ${HOME}/.${entry}
-}
-
-export -f Deploy
-export REPO path
-
-find ${REPO} -maxdepth 1 -type f -name '[^.]*' -exec bash -c 'Deploy "$0"' {} \;
-mv ${HOME}/.ssh_rc ${HOME}/.ssh/rc
-Deploy vim
-
-touch ${HOME}/.friedman.local
-
-mkdir -p ~/scripts
-git clone http://github.com/Alives/tmux-statusline ~/scripts/tmux-statusline
+touch ${HOME}/.zshrc.local
 
 git submodule init
 git submodule update
