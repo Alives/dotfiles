9bd18c2cf7d3656491668cd1f7a6f199e3db8209
Elliott Friedman <elliott.friedman@gmail.com>
2020-08-31 16:24:48 -0400

    [tmux] 1 sec updates more fault tolerant

--- scripts/tmux_statusline.sh
+++ scripts/tmux_statusline.sh
@@ -1,47 +1,56 @@
 #!/bin/bash -e
-DATA=${HOME}/.tmux.data
+DATA=/var/run/tmux.data_${USER}
 NICS=("eth0" "eno1")
 
-# Network
-net="$(cat /proc/net/dev)"
-for nic in "${NICS[@]}"; do
-  curr=($(echo "$net" | awk "/$nic:/ {printf \$2\" \"\$10}"))
-  test "${#curr[@]}" -eq "2" && break
-done
-curr_rx=${curr[0]}
-curr_tx=${curr[1]}
-curr_ts=$(date +%s)
+network_tab () {
+  printf "#[fg=colour27,bg=colour0]#[bg=colour27] "
+  net="$(cat /proc/net/dev)"
+  for nic in "${NICS[@]}"; do
+    curr=($(echo "$net" | awk "/$nic:/ {printf \$2\" \"\$10}"))
+    test "${#curr[@]}" -eq "2" && break
+  done
+  curr_rx=${curr[0]}
+  curr_tx=${curr[1]}
+  curr_ts=$(date +%s)
 
-test -r ${DATA} \
-  && prev=($(cat ${DATA} 2>/dev/null))
-[[ -z "${prev[0]}" || -z "${prev[1]}" || -z "${prev[2]}" ]] \
-  && prev=(${curr_rx} ${curr_tx} ${curr_ts})
-prev_rx=${prev[0]}
-prev_tx=${prev[1]}
-prev_ts=${prev[2]}
-echo "${curr_rx} ${curr_tx} ${curr_ts}" > ${DATA}
+  test -r ${DATA} \
+    && prev=($(cat ${DATA} 2>/dev/null))
+  [[ -z "${prev[0]}" || -z "${prev[1]}" || -z "${prev[2]}" ]] \
+    && prev=(${curr_rx} ${curr_tx} ${curr_ts})
+  prev_rx=${prev[0]}
+  prev_tx=${prev[1]}
+  prev_ts=${prev[2]}
+  echo "${curr_rx} ${curr_tx} ${curr_ts}" > ${DATA}
 
-diff_ts=$((curr_ts-prev_ts))
-diff_rx=$(echo | awk "{printf(\"%f\", (($curr_rx-$prev_rx)/$diff_ts))}")
-diff_tx=$(echo | awk "{printf(\"%f\", (($curr_tx-$prev_tx)/$diff_ts))}")
-rate=($(numfmt --format='%.1fB/s' --to=iec $diff_rx $diff_tx))
-rate_rx="$(echo ${rate[0]} | \
-           sed -r 's/([0-9])([A-Za-z])/\1#[fg=colour249]\2/g')"
-rate_tx="$(echo ${rate[1]} | \
-           sed -r 's/([0-9])([A-Za-z])/\1#[fg=colour249]\2/g')"
-echo -n "#[fg=colour27,bg=colour0]#[bg=colour27] "
-echo -n "#[fg=colour249]↓#[fg=colour255]${rate_rx} "
-echo -n "#[fg=colour249]↑#[fg=colour255]${rate_tx} "
-echo -n "#[fg=colour0,bg=colour27]#[bg=colour0]"
-# Network
+  diff_ts=$((curr_ts-prev_ts))
+  if [[ $diff_ts = 0 ]]; then
+    rate_rx="---.-KB/s"
+    rate_tx="${rate_rx}"
+  else
+    diff_rx=$(echo | awk "{printf(\"%f\", (($curr_rx-$prev_rx)/$diff_ts))}")
+    diff_tx=$(echo | awk "{printf(\"%f\", (($curr_tx-$prev_tx)/$diff_ts))}")
+    rate=($(numfmt --format='%.1fB/s' --to=iec $diff_rx $diff_tx))
+    rate_rx="$(echo ${rate[0]} | \
+               sed -r 's/([0-9])([A-Za-z])/\1#[fg=colour249]\2/g')"
+    rate_tx="$(echo ${rate[1]} | \
+               sed -r 's/([0-9])([A-Za-z])/\1#[fg=colour249]\2/g')"
+  fi
+  printf "#[fg=colour249]↓#[fg=colour255]%24s " "${rate_rx}"
+  printf "#[fg=colour249]↑#[fg=colour255]%24s " "${rate_tx}"
+  printf "#[fg=colour0,bg=colour27]#[bg=colour0]"
+}
 
-# Load
-echo -n "#[bg=colour0]#[fg=colour34,bg=colour0]#[fg=colour255,bg=colour34] "
-awk '{printf $1" "$2" "$3}' /proc/loadavg
-echo -n " #[fg=colour0,bg=colour34]#[fg=colour220,bg=colour0]"
-# Load
+load_tab () {
+  echo -n "#[bg=colour0]#[fg=colour34,bg=colour0]#[fg=colour255,bg=colour34] "
+  awk '{printf $1" "$2" "$3}' /proc/loadavg
+  echo -n " #[fg=colour0,bg=colour34]#[fg=colour220,bg=colour0]"
+}
 
-# Time
-echo -n "#[fg=colour220,bg=colour0]#[fg=colour0,bg=colour220]"
-echo -n "$(date +'%l:%M:%S %p') #[default]"
-# Time
+time_tab () {
+  echo -n "#[fg=colour220,bg=colour0]#[fg=colour0,bg=colour220]"
+  echo -n "$(date +'%l:%M:%S %p') #[default]"
+}
+
+network_tab
+load_tab
+time_tab
