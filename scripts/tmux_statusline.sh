e04ca78a6dbcd75c4f3c9f51c132821f00d9ff99
Elliott Friedman <elliott.friedman@gmail.com>
2020-08-31 14:21:00 -0400

    [tmux] redo statusline in bash

---
+++ scripts/tmux_statusline.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+DATA=${HOME}/.tmux.data
+
+# Network
+curr=($(awk '/eth0:/ {printf $2" "$10}' /proc/net/dev))
+curr_rx=${curr[0]}
+curr_tx=${curr[1]}
+curr_ts=$(date +%s)
+
+test -r ${DATA} \
+  && prev=($(cat ${DATA} 2>/dev/nulli))
+[[ -z "${prev[0]}" || -z "${prev[1]}" || -z "${prev[2]}" ]] \
+  && prev=(${curr_rx} ${curr_tx} ${curr_ts})
+prev_rx=${prev[0]}
+prev_tx=${prev[1]}
+prev_ts=${prev[2]}
+echo "${curr_rx} ${curr_tx} ${curr_ts}" > ${DATA}
+
+diff_ts=$((curr_ts-prev_ts))
+diff_rx=$(echo | awk "{printf(\"%f\", (($curr_rx-$prev_rx)/$diff_ts))}")
+diff_tx=$(echo | awk "{printf(\"%f\", (($curr_tx-$prev_tx)/$diff_ts))}")
+rate=($(numfmt --format='%.1fB/s' --to=iec $diff_rx $diff_tx))
+rate_rx="$(echo ${rate[0]} | \
+           sed -r 's/([0-9])([A-Za-z])/\1#[fg=colour249]\2/g')"
+rate_tx="$(echo ${rate[1]} | \
+           sed -r 's/([0-9])([A-Za-z])/\1#[fg=colour249]\2/g')"
+echo -n "#[fg=colour27,bg=colour0]#[bg=colour27] "
+echo -n "#[fg=colour249]↓#[fg=colour255]${rate_rx} "
+echo -n "#[fg=colour249]↑#[fg=colour255]${rate_tx} "
+echo -n "#[fg=colour0,bg=colour27]#[bg=colour0]"
+# Network
+
+# Load
+echo -n "#[bg=colour0]#[fg=colour34,bg=colour0]#[fg=colour255,bg=colour34] "
+awk '{printf $1" "$2" "$3}' /proc/loadavg
+echo -n " #[fg=colour0,bg=colour34]#[fg=colour220,bg=colour0]"
+# Load
+
+# Time
+echo -n "#[fg=colour220,bg=colour0]#[fg=colour0,bg=colour220]"
+echo -n "$(date +'%l:%M:%S %p') #[default]"
+# Time
