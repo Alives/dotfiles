c3a0bd252d51d29fdab27c384b1f46f82dcbfccb
Elliott Friedman <elliott.friedman@gmail.com>
2020-09-17 23:26:15 -0400

    [tmux_statusline] work with older bash, accept 4 digit network numbers

--- scripts/tmux_statusline.sh
+++ scripts/tmux_statusline.sh
@@ -3,10 +3,11 @@ DATA=${XDG_RUNTIME_DIR}/tmux.data
 NICS=("eth0" "eno1")
 
 network_tab () {
+  declare -a curr prev rate
   printf "#[fg=colour27,bg=colour0]#[bg=colour27] "
   net="$(cat /proc/net/dev)"
   for nic in "${NICS[@]}"; do
-    readarray -d' ' curr < <(echo "$net" | awk "/$nic:/ {printf \$2\" \"\$10}")
+    readarray -t curr < <(echo "$net" | awk "/$nic:/ {printf \$2\"\n\"\$10}")
     test "${#curr[@]}" -eq "2" && break
   done
   curr_rx=${curr[0]}
@@ -36,8 +37,8 @@ network_tab () {
     rate_tx="$(echo "${rate[1]}" | \
                sed -r 's/([0-9])([ A-Za-z])/\1#[fg=colour249]\2/g')"
   fi
-  printf "#[fg=colour249]↓#[fg=colour255]%24s " "${rate_rx}"
-  printf "#[fg=colour249]↑#[fg=colour255]%24s " "${rate_tx}"
+  printf "#[fg=colour249]↓#[fg=colour255]%25s " "${rate_rx}"
+  printf "#[fg=colour249]↑#[fg=colour255]%25s " "${rate_tx}"
   printf "#[fg=colour0,bg=colour27]#[bg=colour0]"
 }
 
